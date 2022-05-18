4bf6c3e4f48485b60887995559afc117f9296376
xboxfly15 <19935166+xboxfly15@users.noreply.github.com>
2022-05-18 19:18:20 +0100

    🚀 v1.1.0 Refactor, update dependencies Update project name + update latest nodejs project boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# njs_rss_to_json
-A little RSS to JSON API application I created for a personal dashboard/organizer I'm making
+# node_rss-to-json
+
+A little RSS to JSON API application I created for a personal dashboard/organizer
--- app.js
+++ app.js
@@ -1,45 +1,49 @@
-'use strict'
-const rssParser = require('rss-parser')
-const parser = new rssParser()
+const RssParser = require('rss-parser')
+const parser = new RssParser()
 const app = require('express')()
-const CronJob = require('cron').CronJob
+const cron = require('node-cron')
 
 const settings = require('./settings.json')
 const refreshRate = parseInt(settings.refreshRateInMinutes)
 const limit = parseInt(settings.limitItemsPerRSSFeed)
-const rssFeeds = settings.rssFeeds
-let rssCache = {}
+const { rssFeeds } = settings
+const rssCache = {}
+
+const ipAddr = process.env.IP_ADDR || '0.0.0.0'
+const port = process.env.PORT || 9000
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  return next()
+})
 
 app.get('/', (req, res) => res.json(rssCache))
 app.get('/feeds', (req, res) => res.json(rssFeeds))
-app.listen(8080, () => console.log('Listening on port 8080'))
+app.listen(port, ipAddr, () => console.info(`node_rss-to-json started on ${ipAddr}:${port}. Refresh rate set at ${refreshRate} minutes & limit set at ${limit} items per RSS feed`))
 
-var job = new CronJob('0 */'+refreshRate+' * * * *', () => {
+cron.schedule(`0 */${refreshRate} * * * *`, () => {
   Object.keys(rssFeeds).forEach(async category => {
-    rssCache[category] = new Array()
+    rssCache[category] = []
     console.log('Fetching ' + category)
     for (const url of rssFeeds[category]) await fetchAndPushItems(rssCache[category], url, limit)
     console.log('Fetched ' + rssCache[category].length + ' items from ' + category)
   })
 })
-job.start()
 
-function fetchAndPushItems(array, url, limit=25) {
+function fetchAndPushItems(array, url, limit = 25) {
   return new Promise(resolve => resolve(parser.parseURL(url)))
-  .then(feed => {
-    for(let i = 0; i < limit; i++) {
-      array.push({
-        title: escapeString(feed.items[i].title),
-        link: escapeString(feed.items[i].link),
-        content: escapeString(feed.items[i].contentSnippet),
-        published: parseInt(new Date(escapeString(feed.items[i].isoDate)).getTime())
-      })
-    }
-  })
+    .then(feed => {
+      for (let i = 0; i < limit; i++) {
+        array.push({
+          title: escapeString(feed.items[i].title),
+          link: escapeString(feed.items[i].link),
+          content: escapeString(feed.items[i].contentSnippet),
+          published: parseInt(new Date(escapeString(feed.items[i].isoDate)).getTime())
+        })
+      }
+    })
 }
 
 function escapeString(str) {
   return str.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;')
 }
-
-console.log('Started njs_rss_to_json, refresh rate set at ' + refreshRate + ' minutes & limit set at ' + limit + ' items per RSS feed')
